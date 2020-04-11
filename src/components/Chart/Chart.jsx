a23c3867b10f93dd6bd40deac09803e9d1621f60
mayurpatel2904 <mayur@justgreen.in>
2020-04-11 18:34:44 +0530

    New Change in Chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -33,8 +33,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
                             fill: true
                         }]
                     }}
-                    width="85%"
-                    height= "70%"
+                    width= {100}
                 />
             ) : null
     )
@@ -55,6 +54,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
                             data:[confirmed.value, recovered.value, deaths.value]
                         }]
                     }}
+                    width= {100}
                     options={{
                         legend: { display: false },
                         title: { display: true, text: `Current state in ${country}`}
