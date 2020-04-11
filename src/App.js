b5d771fe10dcddfb063f9eed25c6ac01fd96da35
mayurpatel2904 <mayur@justgreen.in>
2020-04-11 16:25:38 +0530

    MenuBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css'
 import { fetchData } from './api'
 
 import coronaImage from './images/image.png'
+import { Toolbar, AppBar, IconButton, Typography, Button, MenuItem, FormControlLabel } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
 
 class App extends Component{
 
@@ -32,12 +36,18 @@ class App extends Component{
 
     const { data, country } = this.state
     return(
-      <div className={styles.container}>
-      <img className={styles.image} alt="COVID-19" src={coronaImage} />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country}/>
-      </div>
+      <Fragment>
+      <AppBar position="sticky">
+        <Toolbar>
+          <img className={styles.image} alt="COVID-19" src={coronaImage} />
+        </Toolbar>
+      </AppBar>
+        <div className={styles.container}>
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Cards data={data} />
+          <Chart data={data} country={country}/>
+        </div>
+      </Fragment>
     )
   }
   
