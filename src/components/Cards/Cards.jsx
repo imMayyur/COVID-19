133639237baf7378f77f8d5a3086f4142fd947b4
mayurpatel2904 <mayur@justgreen.in>
2020-04-11 15:04:53 +0530

    Card Changes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -30,7 +30,7 @@ const Cards = ( { data: { confirmed, recovered, deaths, lastUpdate} }) => {
                             <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                        <Typography variant="body2">Number of Recovered cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
@@ -40,7 +40,7 @@ const Cards = ( { data: { confirmed, recovered, deaths, lastUpdate} }) => {
                             <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                        <Typography variant="body2">Number of Deaths due to COVID-19</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
